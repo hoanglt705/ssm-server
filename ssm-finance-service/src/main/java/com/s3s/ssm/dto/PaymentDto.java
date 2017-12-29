e94569200eb574236d5a2a5f397cf6b15f68efd9
hoanglt705 <hoanglt705@gmail.com>
2017-12-29 13:41:06 +0900

    Add finance

---
+++ ssm-finance-service/src/main/java/com/s3s/ssm/dto/PaymentDto.java
@@ -0,0 +1,64 @@
+package com.s3s.ssm.dto;
+
+import java.util.Date;
+
+import com.s3s.ssm.dto.AbstractCodeObject;
+
+public class PaymentDto extends AbstractCodeObject {
+  private static final long serialVersionUID = 1L;
+  private PaymentContentDto paymentContent;
+  private Date paymentDate;
+  private String staff;
+  private PaymentMode paymentMode = PaymentMode.CASH;
+  private Long amount;
+  private String notes;
+
+  public PaymentContentDto getPaymentContent() {
+    return paymentContent;
+  }
+
+  public void setPaymentContent(PaymentContentDto paymentContent) {
+    this.paymentContent = paymentContent;
+  }
+
+  public Date getPaymentDate() {
+    return paymentDate;
+  }
+
+  public void setPaymentDate(Date paymentDate) {
+    this.paymentDate = paymentDate;
+  }
+
+  public String getStaff() {
+    return staff;
+  }
+
+  public void setStaff(String staff) {
+    this.staff = staff;
+  }
+
+  public PaymentMode getPaymentMode() {
+    return paymentMode;
+  }
+
+  public void setPaymentMode(PaymentMode paymentMode) {
+    this.paymentMode = paymentMode;
+  }
+
+  public Long getAmount() {
+    return amount;
+  }
+
+  public void setAmount(Long amount) {
+    this.amount = amount;
+  }
+
+  public String getNotes() {
+    return notes;
+  }
+
+  public void setNotes(String notes) {
+    this.notes = notes;
+  }
+
+}
